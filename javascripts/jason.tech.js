5db2a4e78eb3b8774f157a9761d412715ff7f07f
Jason Bodley <bodleyjason@gmail.com>
2015-08-31 08:58:16 +0100

    lets try this

--- javascripts/jason.tech.js
+++ javascripts/jason.tech.js
@@ -4,17 +4,8 @@ $(function() {
 
     var hour = new Date().getHours();
 
-    var nightMode = localStorage.getItem("night-mode");
     var mainContent = localStorage.getItem("main-content");
     var fontSize = localStorage.getItem("");
-
-    if (nightMode) {
-        if (nightMode === "true") {
-            $("#image").trigger("click");
-        }
-    } else if (hour >= 21 || hour < 6) {
-        $("#image").trigger("click");
-    }
 
     if (!mainContent) {
         var mainContent = localStorage.setItem("main-content", "home");
@@ -32,21 +23,6 @@ $(function() {
 
 
 
-});
-
-$("#image").click(function() {
-    var x = $("html");
-    if (x.hasClass("invert")) {
-        localStorage.setItem("night-mode", false);
-        x.removeClass("invert").addClass("no-invert");
-        $(this).removeClass("invert").addClass("no-invert");
-        //$(".invert-only").stop().fadeOut();
-    } else {
-        //$(".invert-only").stop().fadeIn();
-        x.removeClass("no-invert").addClass("invert");
-        $(this).removeClass("no-invert").addClass("invert");
-        localStorage.setItem("night-mode", true);
-    }
 });
 
 $(".font-change").click(function() {
