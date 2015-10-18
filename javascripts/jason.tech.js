abaa591344369909e6a2f4b1803e7b5a83e9aa9c
Jason Bodley <bodleyjason@gmail.com>
2015-10-18 09:51:48 +0100

    Error handling

--- javascripts/jason.tech.js
+++ javascripts/jason.tech.js
@@ -1,57 +1,97 @@
 $(function() {
-		$("header a").attr("target","_blank");
 
-    var mainContent = localStorage.getItem("main-content");
-    var backgroundImage = localStorage.getItem("background-image");
+  $('.overlay').show();
+  $("header a").attr("target", "_blank");
 
-    if (!mainContent) {
-        var mainContent = localStorage.setItem("main-content", "home");
-        mainContent = "home";
-    }
-    
-    if (backgroundImage) {
-	    $('#' + backgroundImage).trigger("click");
-    }
+  var mainContent = localStorage.getItem("main-content");
+  var backgroundImage = localStorage.getItem("background-image");
 
-    $('#main-content').load(mainContent + '.html', function() {
-        $(".fade-in").hide().fadeIn("slow", function() {
-            $('#' + mainContent).addClass("active");
-            $('.overlay').hide();
-        });
+  if (!mainContent) {
+    var mainContent = localStorage.setItem("main-content", "home");
+    mainContent = "home";
+  }
+
+  if (backgroundImage) {
+    $('#' + backgroundImage).trigger("click");
+  }
+
+  $('#main-content').load(mainContent + '.html', function() {
+  
+    $(".fade-in").hide().fadeIn("slow", function() {
+      $('#' + mainContent).addClass("active");
+      $('.overlay').hide();
+      $('.disabled').removeClass("disabled");
     });
     
+  });
+
 });
 
 $(".img-changer").click(function() {
-	if ($(this).hasClass("fa-square-o")) {
-		$(".img-changer").removeClass("fa-square").addClass("fa-square-o");
-		$(this).addClass("fa-square");
-		var id = $(this).attr("id");
-		$("body").removeClass();
-		$("body").addClass("background-" + id);
-		localStorage.setItem("background-image", id);
-	}
+
+  var $changer = $(this);
+
+  if ($changer.hasClass("fa-square-o")) {
+
+    $(".img-changer").removeClass("fa-square").addClass("fa-square-o");
+    $(this).addClass("fa-square");
+    var id = $(this).attr("id");
+    $("body").removeClass();
+    $("body").addClass("background-" + id);
+    localStorage.setItem("background-image", id);
+
+  }
+
 });
 
 $(".page-changer").click(function() {
 
-		var $mainContent = $('#main-content');
-		var $spinner = $("#nonObtrusiveSpinner");
-		
-		$spinner.show();
-    
+  var $changer = $(this);
+
+  if (!$changer.hasClass("active") && !$changer.hasClass("disabled")) {
+
+    var $mainContent = $('#main-content');
+    var $spinner = $("#nonObtrusiveSpinner");
+
+    $spinner.show();
+
     $(".page-changer").removeClass("active");
-    $(this).addClass("active");
-    var id = $(this).attr("id");
+    $(".page-changer").addClass("disabled");
+    $changer.addClass("active");
+    var id = $changer.attr("id");
+    
+    var finishLoading = function(content) {
+    
+	    $mainContent.show();
+      $mainContent.find(".fade-in").hide().fadeIn(function() {});
+      $spinner.hide();
+      $(".page-changer").removeClass("disabled");
+      
+    };
 
     $mainContent.find(".fade-in").fadeOut(function() {
-        $mainContent.empty();
+    
+      $mainContent.empty();
+
+      $mainContent.load(id + '.html', function(response, status, xhr) {
+        if (status == "success") {
+          localStorage.setItem("main-content", id);
+          finishLoading($mainContent);
+        } else {
+          $mainContent.load('error.html', function(response, status, xhr) {
+            $mainContent.find("#verboseError").html("Couldn't find the page '<em>" + spaceOutText(id) + "</em>' - sorry.");
+            finishLoading($mainContent);
+          });
+        }
         
-        $mainContent.load(id + '.html', function(x) {
-            localStorage.setItem("main-content", id);
-            $mainContent.show();
-            $mainContent.find(".fade-in").hide().fadeIn(function() {});
-            $spinner.hide();
-        });
+      });
+      
     });
+
+  }
 });
+
+function spaceOutText(text) {
+	return text.replace(/([A-Z])/g, ' $1')
+		.replace(/^./, function(str){ return str.toUpperCase(); })
+}
